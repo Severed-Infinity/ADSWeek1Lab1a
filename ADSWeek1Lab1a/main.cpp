466f306bf40c97c55ce345c2c6b18dcd7317798c
david swift <davidswift@davids-MacBook-Pro.local>
2015-02-05 10:38:17 +0000

    Initial Commit

---
+++ ADSWeek1Lab1a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ADSWeek1Lab1a
+//
+//  Created by david swift on 05/02/2015.
+//  Copyright (c) 2015 david swift. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
